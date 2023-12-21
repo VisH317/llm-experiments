f33b9ba51166be28dfb467569bea43cd2161f7a9
VisH317 <vt201916384@gmail.com>
2023-12-21 15:28:57 -0500

    testing sparse mha (setting up proper batch inference)

--- sparse_attention_heads/README.md
+++ sparse_attention_heads/README.md
@@ -1,3 +1,5 @@
 ## Sparse Attention Heads
 
 _Moe but for attention heads to scale them without crazy compute constraints_
+
+**TODO:** try with chunking and passing into each head this way, or try clusters
--- sparse_attention_heads/modules/sparse_mha.py
+++ sparse_attention_heads/modules/sparse_mha.py
@@ -27,8 +27,12 @@ class AttentionHead(nn.Module):
 
         print(Q.size(), ", ", K.size())
 
-        QK = torch.matmul(Q, K.transpose(-2, -1)) / torch.sqrt(torch.Tensor(self.att_dim))
-        score = F.softmax(QK)
+        K_T = K.transpose(-2, -1) 
+
+        QK = torch.matmul(Q, K_T)
+        print(QK.size())
+        QK_scaled = QK / (self.att_dim ** 0.5)
+        score = QK_scaled.softmax(-1)
 
         return torch.matmul(score, V)
 
@@ -46,33 +50,45 @@ class SparseMultiHeadAttention(nn.Module):
         if self.route_type == RouteType.att:
             self.att = nn.Linear(d_model, 1)
 
+        self.query = nn.Linear(d_model, d_attn * n_head) # normally each head takes a section of the total embedding input, here we're splitting it up more
+        self.key = nn.Linear(d_model, d_attn * n_head) # TODO: try with chunking and passing into each head this way, or try clusters
+        self.value = nn.Linear(d_model, d_attn * n_head) # TODO: fix this to parallelize
+
         self.router = nn.Linear(d_model, n_head) # need to experiment with pooling in this layer here (max pooling, sum pooling, or additive attention)
         self.w_O = nn.Linear(d_attn * n_active, d_model)
 
     def forward(self, input: Tensor) -> Tensor:
+        # input size: (batch_size, seq_len, d_model)
         dist = self.route(input) # returns dim (batch_size, n_heads)
         sparse_dist_val = torch.topk(dist, self.n_active, -1) # returns dim (batch_size, n_active)
         sparse_dist_idx = sparse_dist_val.indices
+
+        Q = self.query(input).reshape(input.size()[0], input.size()[1], self.n_head, self.d_attn).permute(0, 2, 1, 3) # (batch, head, seq_len, d_model)
+        K = self.key(input).reshape(input.size()[0], input.size()[1], self.n_head, self.d_attn).permute(0, 2, 1, 3)
+        V = self.value(input).reshape(input.size()[0], input.size()[1], self.n_head, self.d_attn).permute(0, 2, 1, 3)
+
+
+
         # TODO: test if you need to insert an extra softmax for the sparse dist here so it adds up to one value (or layernorm fixes this?)
 
         sparse_dist = torch.zeros_like(dist).scatter(-1, sparse_dist_idx, dist)
         sparse_dist = sparse_dist.softmax(1) # softmax not confirmed
 
-        outputs = torch.empty((input.size()[0], self.n_active, input.size()[-2], input.size()[-1]))
+        outputs = torch.empty((input.size()[0], self.n_active, input.size()[-2], self.d_attn))
 
         for ix in range(input.size()[0]):
-            outputs[ix] = self.compute_head(input[ix], sparse_dist[ix])
+            outputs[ix] = self.compute_head(input[ix], sparse_dist[ix], sparse_dist_idx[ix])
 
         return self.w_O(torch.sum(outputs, -2))
 
     # router functions
 
-    def compute_head(self, input: Tensor, sparse_dist: Tensor) -> Tensor:
-        outputs = torch.empty((self.n_active, input.size()[-2], input.size()[-1]))
+    def compute_head(self, input: Tensor, sparse_dist: Tensor, sparse_dist_idx: Tensor) -> Tensor:
+        outputs = torch.empty((self.n_active, input.size()[-2], self.d_attn))
 
-        for ix, head in enumerate(self.heads):
-            print(sparse_dist.size())
-            outputs[ix] = head(input) * sparse_dist[ix] if sparse_dist[ix] != 0 else torch.zeros_like(input)
+        for ix in sparse_dist_idx:
+            print(list(sparse_dist_idx))
+            outputs[ix] = self.heads[ix](input) * sparse_dist[ix] # if sparse_dist[ix] != 0 else torch.zeros(input.size()[-2], self.d_attn)
 
         return outputs
 
