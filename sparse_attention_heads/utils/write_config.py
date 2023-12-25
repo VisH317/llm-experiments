0db3714af59b78218a8b96cdc9506addfe62903e
VisH317 <vt201916384@gmail.com>
2023-12-24 19:20:46 -0500

    config util

---
+++ sparse_attention_heads/utils/write_config.py
@@ -0,0 +1,10 @@
+from configparser import ConfigParser
+
+def write_config(path: str, d: dict):
+    config = ConfigParser()
+    config["train"] = d["train"]
+    config["transformer"] = d["transformer"]
+    config["vocab"] = d["vocab"]
+
+    with open(path) as f:
+        config.write(f)
